f6e121ad783bea85fda94411cb3bf7508a922a55
Tess Myers <tess@tessmyers.com>
2015-07-22 20:11:24 -0700

    Dump main files + server

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var fs = require('fs')
+var app = express();
+
+// Very likely going to replace this with fully static file server, and control the content via path
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/contents', function(req, res){
+
+  fs.readFile(req.url, function(err, data){
+    if (err) {
+      console.log('error!', err);
+    } else {
+      res.end(data);
+    }
+  })
+})
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
